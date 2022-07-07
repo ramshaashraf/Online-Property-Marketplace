f724d44a9129d2064b639d4b74cebbb0b7301b17
ramshaashraf <ramshaashraf7@gmail.com>
2022-07-07 20:36:31 +0530

    Initial Commit

---
+++ src/pic1.java
@@ -0,0 +1,20 @@
+
+import javax.swing.ImageIcon;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nazim Ashraf
+ */
+class pic1 {
+
+    static void setIcon(ImageIcon newIcon) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/ta1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nazim Ashraf
+ */
+class ta1 {
+
+    static void append(String address_Lohegaon_road_Wagholi) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
